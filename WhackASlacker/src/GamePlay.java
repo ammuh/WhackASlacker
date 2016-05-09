14117401aec5de70c4cb26c43061051f3d5e8cf3
Ammar Husain <ammo700@gmail.com>
2016-05-09 07:30:45 -0700

    threads working in parallel

--- WhackASlacker/src/GamePlay.java
+++ WhackASlacker/src/GamePlay.java
@@ -1,37 +1,75 @@
 /**
  * Created by ammu on 5/7/16.
  */
+import com.sun.xml.internal.ws.api.*;
+
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
+import javax.swing.border.*;
 
 public class GamePlay{
     //Game Stats
-    private volatile int score;
-    private volatile int time;
+    public volatile int score;
+    public volatile int time;
     private JFrame frame;
     private boolean gameRunning;
+    private JTextArea timeField;
+    private JTextArea scoreField;
+    private JLabel[] holes = new JLabel[12];
+    private Hole[] holeThreads = new Hole[12];
 
     public GamePlay(JFrame f){
         frame = f;
         score = 0;
         time = 30;
+
+        JPanel info = new JPanel(new FlowLayout(FlowLayout.CENTER, 30, 10));
+
+        //Info Layout
+        info.setBorder(new EmptyBorder(30, 30, 30, 30));
+
+        info.add(Box.createHorizontalStrut(100));
+
+        JLabel timeLabel = new JLabel("Time Left:");
+        info.add(timeLabel);
+
+        timeField = new JTextArea(1, 5);
+        timeField.setEditable(false);
+        info.add(timeField);
+        timeField.setVisible(true);
+
+        JLabel scoreLabel = new JLabel("Score:");
+        info.add(scoreLabel);
+
+        scoreField = new JTextArea(1, 5);
+        scoreField.setEditable(false);
+        info.add(scoreField);
+        scoreField.setVisible(true);
+
+        JPanel game = new JPanel(new BorderLayout());
+        JPanel grid = new JPanel(new GridLayout(0,4));
+
+        for (int i = 0; i < 12; i++){
+            JLabel j = new JLabel("     ");
+            j.setHorizontalAlignment(JLabel.CENTER);
+            holes[i] = j;
+            grid.add(j);
+            holeThreads[i] = new Hole(j, frame, this);
+        }
+
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-        JPanel info = new JPanel(new FlowLayout());
-        JPanel game = new JPanel(new BorderLayout());
-        JPanel grid = new JPanel(new GridLayout(0,3));
-        game.add(grid);
+        game.add(grid, BorderLayout.CENTER);
         panel.add(info);
         panel.add(game);
 
-        info.add(new JLabel("Game Has Started"));
-        for (int i = 0; i < 10; i++){
-            grid.add(new JLabel("Element" + (i+1)));
-        }
-
+        //Frame Init
         frame.setContentPane(panel);
         frame.setVisible(true);
         gameRunning = true;
+        begin();
     }
     public void begin(){
        /* while(time > 30 && gameRunning){
@@ -39,6 +77,40 @@ public class GamePlay{
         }*/
 
 
+        Thread timerThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int temp = time;
+                while (time > -1) {
+
+                    try {
+                        timeField.setText("" + time);
+                        time--;
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+                gameEnd();
+
+            }
+        });
+
+        timerThread.start();
+
+        for (Hole h : holeThreads) {
+            h.start();
+        }
+    }
+    public void gameEnd(){
+        for (Hole h : holeThreads) {
+            h.interrupt();
+        }
+        /*
+        time = 30;
+        score = 0;
+        timeField.setText("" + time);
+        scoreField.setText("" + score);*/
     }
 
 }
--- WhackASlacker/src/Hole.java
+++ WhackASlacker/src/Hole.java
@@ -3,33 +3,51 @@ import javax.swing.*;
 /**
  * Created by ammu on 5/8/16.
  */
-public class Hole {
-    private JPanel hPanel;
+public class Hole extends Thread{
+    private JLabel hPanel;
     private JFrame mframe;
-    private Character[] characters = {new Ammar(), new Auni(), new Ani()};
-    public Hole(JPanel hole, JFrame main){
+    private GamePlay game;
+    private Character[] characters = {};
+    public Hole(JLabel hole, JFrame main, GamePlay g){
         hPanel = hole;
         mframe = main;
+        game = g;
     }
-    public void loop(){
-        while(true){
-            pop();
+
+    public void run(){
+        while(game.time > -1){
             try {
-                Thread.sleep((int)Math.random()*4000);
+                Thread.sleep((int)Math.random()*3000 +2);
             } catch (InterruptedException e) {
                 // Should not happen
                 throw new AssertionError(e);
             }
+            synchronized (hPanel){
+                this.hPanel.setText(" POP ");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    // Should not happen
+                    throw new AssertionError(e);
+                }
+                this.hPanel.setText("     ");
+            }
+
+
+
+        /*
+        Character c = characters[(int)Math.random()*characters.length];
+        //hPanel = new JPanel();
+        mframe.setVisible(true);*/
+
         }
     }
-    public void pop(){
-        Character c = characters[(int)Math.random()*characters.length];
-        hPanel = new JPanel();
-        mframe.setVisible(true);
+
+    public void loop(){
 
     }
-
     private void hitSound(Character c){
-        
+
     }
+
 }
